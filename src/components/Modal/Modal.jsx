0bf8d4d58f828bc379c2469b49b6c60d045ca3f0
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-23 17:01:59 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,49 +1,45 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 //import styles from './Modal.module.css';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+//import { Component } from 'react';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 const domNode = document.querySelector('#root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEsc);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEsc);
-  }
-
-  handleEsc = evt => {
+export const Modal = ({ onCloseModal, imageUrl, alt }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  });
+  const handleEsc = evt => {
     if (evt.code === 'Escape') {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
 
-  render() {
-    const { onCloseModal, imageUrl, alt } = this.props;
-    return createPortal(
-      <Overlay 
-        onClick={event => {
-          if (event.target !== event.currentTarget) {
-            return;
-          }
-          onCloseModal();
-        }} domNode
-      >
-        <ModalWindow>
-          <img src={imageUrl} alt={alt} />
-        </ModalWindow>
-      </Overlay>,
+  return createPortal(
+    <Overlay
+      onClick={event => {
+        if (event.target !== event.currentTarget) {
+          return;
+        }
+        onCloseModal();
+      }}
       domNode
-    );
-  }
-}
+    >
+      <ModalWindow>
+        <img src={imageUrl} alt={alt} />
+      </ModalWindow>
+    </Overlay>,
+    domNode
+  );
+};
 
 Modal.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   onCloseModal: PropTypes.func.isRequired,
 };
-
